2e20159193ba8572bd5c415f355c21fa95126f9d
Vivek Kumar <94839920+vivekkr6@users.noreply.github.com>
2022-09-04 12:54:04 +0530

    disabled caching

--- Video.js
+++ Video.js
@@ -263,7 +263,7 @@ export default class Video extends Component {
   render() {
     const resizeMode = this.props.resizeMode;
     const source = resolveAssetSource(this.props.source) || {};
-    const shouldCache = !source.__packager_asset;
+    const shouldCache = false;
 
     let uri = source.uri || '';
     if (uri && uri.match(/^\//)) {
